9d555fa26b101858b426bbf187769dc7a7355328
Vijay Gohel <vijay.gohel@veeam.com>
2024-06-27 00:38:36 +0530

    add Composite

---
+++ Composite/Group.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    internal class Group : Component
+    {
+        List<Component> components = new List<Component>();
+
+        public void Add(Component component)
+        {
+            components.Add(component);
+        }
+
+        public void Render()
+        {
+            foreach (var component in components)
+            {
+                component.Render();
+            }
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Composite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group1 = new Group();
+            group1.Add(new Shape());
+            group1.Add(new Shape());
+            
+            Group group2 = new Group();
+            group2.Add(new Shape());
+            group2.Add(new Shape());
+
+            Group group3 = new Group();
+            group3.Add(group1);
+            group3.Add(group2);
+
+            group3.Render();
+            Console.ReadLine();
+        }
+    }
+}
