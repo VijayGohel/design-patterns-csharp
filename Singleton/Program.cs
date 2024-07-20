a22950009efecba402de69ea528ca6f995123120
Vijay Gohel <vijay.gohel@veeam.com>
2024-07-20 16:23:14 +0530

    add Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => { InstantiateSingleton("t1"); });
+            Thread t2 = new Thread(() => { InstantiateSingleton("t2"); });
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+        }
+
+        public static void InstantiateSingleton(string value)
+        {
+            Singleton singleton = Singleton.GetSingleton(value);
+            Console.WriteLine(singleton.value);
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        public string value {  get; private set; }
+
+        private static readonly object _lock = new object();
+
+        private Singleton() { }
+
+        public static Singleton GetSingleton(string value) 
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                        _instance.value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+    }
+}
